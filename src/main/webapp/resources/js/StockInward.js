a6f2949765221362a2b0f81fa8023b1d7d38f561
John Tran <johnanhquan@gmail.com>
2015-05-17 21:18:49 +0700

    Revert "change StockInward view"
    This reverts commit 06317cdcd5f79e4c0bd6abebba895f19d652a3cb.

--- src/main/webapp/resources/js/StockInward.js
+++ src/main/webapp/resources/js/StockInward.js
@@ -2,7 +2,8 @@
  * 
  */
 $(document).ready(function() {
-    	$('#stock_table').dataTable({
+    $(function() {
+        $('#stock_table').dataTable({
             "bProcessing": true,
             "sAjaxSource": "resources/data/stock_inward.txt",
             "aoColumns": [{
@@ -24,11 +25,7 @@ $(document).ready(function() {
                 "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Vietnamese.json"
             }
         });
-    	
-    // DataTable
-
-//    	$("#sumAmount").val(table.column( 4 ).data().sum());
-    
+    });
     
     // provider Name selector
     $("#select_providerN").change(function(){
@@ -46,23 +43,14 @@ $(document).ready(function() {
     	$("select#select_providerN").val(providerName);
     });
     
-    // setting current date for Ngay Nhap
     var date = new Date();
+
     var month = date.getMonth()+1;
     var day = date.getDate();
+
     var output = date.getFullYear() + '/' +
         ((''+month).length<2 ? '0' : '') + month + '/' +
         ((''+day).length<2 ? '0' : '') + day;
-    $('#ngayNhap').val(output);
     
-	var table = $('#stock_table').DataTable();
-	$('#sumAmount').val(table.column( 4 ).data().sum());
+    $('#ngayNhap').val(output);
 });
-
-$.fn.dataTable.Api.register( 'column().data().sum()', function () {
-	return this.reduce( function (a, b) {
-		var x = parseFloat( a ) || 0;
-		var y = parseFloat( b ) || 0;
-		return x + y;
-	} );
-} );
