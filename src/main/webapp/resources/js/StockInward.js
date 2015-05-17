370af386ac570b0051d906ebeb3e7e6ca2431c60
ThanhThai <n.thanhthai3010@hotmail.com>
2015-05-17 23:01:45 +0700

    change jquery

--- src/main/webapp/resources/js/StockInward.js
+++ src/main/webapp/resources/js/StockInward.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
     	$('#stock_table').dataTable({
             "bProcessing": true,
-            "sAjaxSource": "resources/data/stock_inward.txt",
+            "sAjaxSource": "/getProduct",
             "aoColumns": [{
                 "mData": "code"
             }, {
@@ -22,9 +22,40 @@ $(document).ready(function() {
             }],
             "language": {
                 "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Vietnamese.json"
+            },
+            "footerCallback": function ( row, data, start, end, display ) {
+                var api = this.api(), data;
+     
+                // Remove the formatting to get integer data for summation
+                var intVal = function ( i ) {
+                    return typeof i === 'string' ?
+                        i.replace(/[\$,]/g, '')*1 :
+                        typeof i === 'number' ?
+                            i : 0;
+                };
+     
+                // Total over all pages
+                total = api
+                    .column( 4 )
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    } );
+     
+                // Total over this page
+                pageTotal = api
+                    .column( 4, { page: 'current'} )
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    }, 0 );
+     
+                // Update footer
+                $( api.column( 4 ).footer() ).html(
+                    '$'+pageTotal +' ( $'+ total +' total)'
+                );
             }
         });
-    	
     // DataTable
 
 //    	$("#sumAmount").val(table.column( 4 ).data().sum());
